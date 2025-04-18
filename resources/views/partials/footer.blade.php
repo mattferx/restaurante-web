bb853edc059ad9f9a21db48f83216a9babe9a889
Mattias Cabada <mattias.fernando14@gmail.com>
2025-04-17 21:47:47 -0500

    Agregado y editado footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,79 @@
+
+{{-- <footer class="footer section has-bg-image text-center" style="">
+    <div class="container">
+
+      <!-- Bloque superior: 3 columnas (logo+contacto, menú, redes) -->
+      <div class="footer-top grid-list">
+
+        <!-- Columna 1 - Branding y contacto -->
+        <div class="footer-brand has-before has-after">
+          <a href="#" class="logo">
+            <img src="/images/mirazurrojo.svg" width="160" height="50" loading="lazy" alt="Mirazur Restaurante">
+          </a>
+
+          <!-- Información de contacto -->
+          <address class="body-4">
+            📍 Carrer del Bisbe, 08002 Barcelona, España
+          </address>
+          
+          <!-- Enlaces de contacto directo -->
+          <a href="reservas@mirazur.com" class="body-4 contact-link">reservas@mirazur.com</a>
+          <a href="tel:+51902106989" class="body-4 contact-link">Reservas: +51 902 106 989</a>
+
+          <!-- Horario de atención -->
+          <p class="body-4">
+            Horario: 09:00 am - 01:00 pm
+          </p>
+
+          <!-- Separadores decorativos -->
+          <div class="wrapper">
+            <div class="separator"></div>
+            <div class="separator"></div>
+            <div class="separator"></div>
+          </div>
+
+          <!-- Formulario de newsletter -->
+          <p class="title-1">Recibe Noticias y Ofertas</p>
+          <p class="label-1">
+            Suscríbete y obtén <span class="span">25% de descuento</span>
+          </p>
+
+          <form action="/newsletter" method="POST" class="input-wrapper">
+            <div class="icon-wrapper">
+              <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
+              <input type="email" name="email" placeholder="Tu correo electrónico" required class="input-field">
+            </div>
+            <button type="submit" class="btn btn-secondary">
+            </button>
+          </form>
+        </div>
+
+        <!-- Columna 2 - Menú principal -->
+        <ul class="footer-list">
+          <li><a href="#inicio" class="label-2 footer-link hover-underline">Inicio</a></li>
+          <li><a href="#menu" class="label-2 footer-link hover-underline">Menús</a></li>
+          <li><a href="#nosotros" class="label-2 footer-link hover-underline">Nosotros</a></li>
+          <li><a href="#chefs" class="label-2 footer-link hover-underline">Nuestros Chefs</a></li>
+          <li><a href="#contacto" class="label-2 footer-link hover-underline">Contacto</a></li>
+        </ul>
+
+        <!-- Columna 3 - Redes sociales -->
+        <ul class="footer-list">
+          <li><a href="https://facebook.com/mirazur" target="_blank" class="label-2 footer-link hover-underline">Facebook</a></li>
+          <li><a href="https://instagram.com/mirazur" target="_blank" class="label-2 footer-link hover-underline">Instagram</a></li>
+          <li><a href="https://twitter.com/mirazur" target="_blank" class="label-2 footer-link hover-underline">Twitter</a></li>
+          <li><a href="https://youtube.com/mirazur" target="_blank" class="label-2 footer-link hover-underline">YouTube</a></li>
+          <li><a href="https://maps.app.goo.gl/mirazur" target="_blank" class="label-2 footer-link hover-underline">Google Maps</a></li>
+        </ul>
+
+      </div>
+
+      <!-- Bloque inferior - Copyright -->
+      <div class="footer-bottom">
+        <p class="copyright">
+          &copy; 2025 Mirazur. Todos los derechos reservados
+        </p>
+      </div>
+
+    </div>
+  </footer> --}}
